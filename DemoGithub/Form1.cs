c9c809ddb31e62e9eb8b84c50fc6fe7e0d4d28dc
Onnur <72163294+Onnur@users.noreply.github.com>
2020-10-01 11:52:49 +0200

    Testar en edit på form1 class.

--- DemoGithub/Form1.cs
+++ DemoGithub/Form1.cs
@@ -16,5 +16,12 @@ namespace DemoGithub
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int i = 10;
+
+
+        }
     }
 }
